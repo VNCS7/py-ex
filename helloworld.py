8681d9fe1e8f743c043611884b6dcd72025e6e1b
VNCS7 <vcalves29@gmail.com>
2018-03-30 22:54:48 -0300

    Hello World, só pra garantir :D

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#Hello World é a frase mais usada por aprendizes de nerd programador.
+# Acredita-se que foi criado por Bill Gates no ano de 1900 e guaraná com rolha e,
+# devido à sua influência entre os nerds, logo se espalhou como uma praga.
+#  A linguagem C++ foi criada exclusivamente para a sintaxe dessa frase, mas mesmo assim esta continua complicada até hoje
+# FONTE: http://desciclopedia.org/wiki/Hello_World
+
+print('Olá, Mundo!')
